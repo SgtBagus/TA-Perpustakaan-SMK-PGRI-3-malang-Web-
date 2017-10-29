fe0a46c4a902fb1c4873876e7c1608e76a24463a
SgtBagus <procw57@gmail.com>
2017-10-29 08:11:02 +0700

    Menu Active

--- menu/sidebar.php
+++ menu/sidebar.php
@@ -3,7 +3,13 @@
   <div class="custom-scrollbar">
     <ul class="sidebar-menu">
       <li class="menu-title">Menu</li>
-      <li>
+      <?php
+      if($page == "DASHBOARD"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="index.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-view-dashboard"></i>
@@ -11,7 +17,13 @@
           <span class="menu-text">Dasboard</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "PEMINJAMAN"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="peminjaman.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-assignment"></i>
@@ -19,7 +31,13 @@
           <span class="menu-text">Peminjaman</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "PENGEMBALIAN"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="pengembalian.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-assignment-returned"></i>
@@ -27,7 +45,13 @@
           <span class="menu-text">Pengembalian</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "BUKU"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="buku.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-book"></i>
@@ -35,7 +59,13 @@
           <span class="menu-text">Buku</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "SANKSI"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="sanksi.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-info"></i>
@@ -43,7 +73,13 @@
           <span class="menu-text">Sanksi</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "USER"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="user.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-account"></i>
@@ -51,7 +87,17 @@
           <span class="menu-text">User</span>
         </a>
       </li>
-      <li class="with-sub">
+      <?php
+      if($page == "SISWA"){
+        echo '<li class="with-sub active open">';
+      }else if ($page == "PEGAWAI"){
+        echo '<li class="with-sub active open">';
+      }else if ($page == "JENIS BUKU"){
+        echo '<li class="with-sub active open">';
+      }else{
+        echo '<li class="with-sub">';
+      }
+      ?>
         <a href="#" aria-haspopup="true">
           <span class="menu-icon">
             <i class="zmdi zmdi-storage"></i>
@@ -59,13 +105,46 @@
           <span class="menu-text">Master</span>
         </a>
         <ul class="sidebar-submenu collapse">
-          <li class="menu-subtitle">Master</li>
-          <li><a href="siswa.php">Siswa</a></li>
-          <li><a href="pegawai.php">Pegawai</a></li>
-          <li><a href="jenis_buku.php">Jenis Buku</a></li>
+          <li class="menu-subtitle">Master</li>   
+          <?php
+          if($page == "SISWA"){
+            echo '<li class="active">';
+          }
+          else {
+            echo '<li>';
+          }
+          ?>
+            <a href="siswa.php">Siswa</a>
+          </li>   
+          <?php
+          if($page == "PEGAWAI"){
+            echo '<li class="active">';
+          }
+          else {
+            echo '<li>';
+          }
+          ?>
+            <a href="pegawai.php">Pegawai</a>
+          </li>
+          <?php
+          if($page == "JENIS BUKU"){
+            echo '<li class="active">';
+          }
+          else {
+            echo '<li>';
+          }
+          ?>
+            <a href="jenis_buku.php">Jenis Buku</a>
+          </li>
         </ul>
       </li>
-      <li>
+      <?php
+      if($page == "CETAK"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="cetak.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-print"></i>
@@ -73,7 +152,13 @@
           <span class="menu-text">Cetak Laporan</span>
         </a>
       </li>
-      <li>
+      <?php
+      if($page == "RIWAYAT"){
+        echo '<li class="active">';
+      }else{
+        echo '<li>';
+      }
+      ?>
         <a href="riwayat.php">
           <span class="menu-icon">
             <i class="zmdi zmdi-time-restore-setting"></i>
